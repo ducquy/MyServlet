a522a40ed52daf5df5bde846526eb01d81281fb7
quynd1 <quynd1@HCM00021.nttdata.com.vn>
2015-11-19 09:01:12 +0700

    Complete log filter

--- MyServlet/src/quynd/learning/filter/LogFilter.java
+++ MyServlet/src/quynd/learning/filter/LogFilter.java
@@ -1,6 +1,8 @@
 package quynd.learning.filter;
 
 import java.io.IOException;
+import java.util.Date;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -8,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Servlet Filter implementation class LogFilter
@@ -19,14 +22,14 @@ public class LogFilter implements Filter {
      * Default constructor. 
      */
     public LogFilter() {
-        // TODO Auto-generated constructor stub
+    	System.out.print("Log filter init!");
     }
 
 	/**
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.print("Log filter destroyed!");
 	}
 
 	/**
@@ -36,6 +39,11 @@ public class LogFilter implements Filter {
 		// TODO Auto-generated method stub
 		// place your code here
 
+		HttpServletRequest req  = (HttpServletRequest) request;
+		String path =  req.getServletPath();
+		
+		// Do log
+		System.out.println("#INFOR: " +  new Date() +" - Servlet path: "+ path + ", URL = " + req.getRequestURL());
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
 	}
